49f7fab8a4f8cca7f5dd571e78f11d3082bc4b7d
Amit <neosoft@macpcs-MacBook-Pro.local>
2021-06-09 21:09:34 +0530

    ocr implemented

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
@@ -14,6 +13,14 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'OCR',home:OCRPage());
+    return MaterialApp(
+      title: 'OCR',
+      debugShowCheckedModeBanner: false,
+      home: OCRPage(),
+      theme: ThemeData(
+        primaryColor: Color(0xff48AA8B),
+        accentColor: Color(0xff57D483),
+      ),
+    );
   }
 }
--- lib/ocr_page.dart
+++ lib/ocr_page.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:simple_ocr_plugin/simple_ocr_plugin.dart';
@@ -15,86 +17,210 @@ class OCRPage extends StatefulWidget {
 class _OCRPageState extends State<OCRPage> {
   OcrModel _result;
   PickedFile _pickedImageFile;
-
+  File _imageFile;
+  TextEditingController _editTextController;
   bool _saving = false;
-
-  Future<void> extractText() async {
-    if (_pickedImageFile == null) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('Null Image')));
-    }
-    try {
-      setState(() {
-        _saving = true;
-      });
-      String _resultStringRaw = await SimpleOcrPlugin.performOCR(
-          _pickedImageFile.path,
-          delimiter: ' ');
-      _result = ocrModelFromJson(_resultStringRaw);
-
-      setState(() {
-        _saving = false;
-      });
-    } catch (e) {
-      print("exception on OCR operation: ${e.toString()}");
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
+    var appBar = AppBar(title: Text('OCR'));
+    var _mediaQuery = MediaQuery.of(context);
+    var _heightOfScreen = _mediaQuery.size.height -
+        appBar.preferredSize.height -
+        _mediaQuery.padding.top -
+        _mediaQuery.padding.bottom;
+
+
+
     return Scaffold(
-      appBar: AppBar(title:Text('OCR')),      floatingActionButton: FloatingActionButton(
-          onPressed: extractText, child: Icon(Icons.search)),
+      appBar: appBar,
+      floatingActionButton: _floatingActionButtons(),
+    
       body: ModalProgressHUD(
         inAsyncCall: _saving,
-        child: Center(
-          child: ListView(
-            reverse: true,
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              SizedBox(height: 50),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Align(
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        _pickedImageFile = await ImagePicker()
-                            .getImage(source: ImageSource.camera);
-                        setState(() {});
-                      },
-                      child: Text('Pick From Camera'),
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                  Align(
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        _pickedImageFile = await ImagePicker()
-                            .getImage(source: ImageSource.gallery);
-                        setState(() {});
-                      },
-                      child: Text('Pick From Gallery'),
-                    ),
-                  )
-                ],
-              ),
-              _pickedImageFile == null
-                  ? Text('Nothing Selected', textAlign: TextAlign.center)
+              _imageFile == null ? SizedBox(height: 300) : Container(),
+              _imageFile == null
+                  ? Center(
+                      child: Text(
+                        'No Images Chosen',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    )
                   : Container(
-                      width: 100,
-                      height: 100,
-                      child: Image.file(File(_pickedImageFile.path))),
-              Divider(),
-              Container(
-                padding: EdgeInsets.all(8.0),
-                child:
-                    Text(_result?.text ?? 'null', textAlign: TextAlign.center),
-              )
+                      height: _heightOfScreen,
+                      width: _mediaQuery.size.width,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 12, vertical: 24),
+                      child: Image.file(_imageFile)),
             ],
           ),
         ),
       ),
+    );
+  }
+/// FloatingActionButtons according to image null status
+  Column _floatingActionButtons() {
+    return _imageFile != null
+        ? Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton.extended(
+                  heroTag: null,
+                  onPressed: () => setState(() {
+                        _imageFile = null;
+                      }),
+                  label: Text('Close'),
+                  icon: Icon(Icons.close)),
+              SizedBox(height: 16),
+              FloatingActionButton.extended(
+                  heroTag: null,
+                  onPressed: () => _cropImage().then((_) => setState(() {})),
+                  label: Text('Crop'),
+                  icon: Icon(Icons.crop)),
+              SizedBox(height: 16),
+              FloatingActionButton.extended(
+                heroTag: null,
+                onPressed: extractText,
+                label: Text('Scan'),
+                icon: Icon(Icons.search),
+              ),
+            ],
+          )
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton.extended(
+                  heroTag: null,
+                  onPressed: () async {
+                    _pickedImageFile = await ImagePicker()
+                        .getImage(source: ImageSource.gallery);
+                    if (_pickedImageFile != null) {
+                      _imageFile = File(_pickedImageFile.path);
+                    }
+                    setState(() {});
+                  },
+                  label: Text('Gallery'),
+                  icon: Icon(Icons.photo)),
+              SizedBox(height: 16),
+              FloatingActionButton.extended(
+                heroTag: null,
+                onPressed: () async {
+                  _pickedImageFile =
+                      await ImagePicker().getImage(source: ImageSource.camera);
+                  if (_pickedImageFile != null) {
+                      _imageFile = File(_pickedImageFile.path);
+                    }
+                  setState(() {});
+                },
+                label: Text('Camera'),
+                icon: Icon(Icons.camera_alt),
+              ),
+            ],
+          );
+  }
+
+  Future<Null> _cropImage() async {
+    File croppedFile = await ImageCropper.cropImage(
+        sourcePath: _pickedImageFile.path,
+        aspectRatioPresets: Platform.isAndroid
+            ? [
+                CropAspectRatioPreset.square,
+                CropAspectRatioPreset.ratio3x2,
+                CropAspectRatioPreset.original,
+                CropAspectRatioPreset.ratio4x3,
+                CropAspectRatioPreset.ratio16x9
+              ]
+            : [
+                CropAspectRatioPreset.original,
+                CropAspectRatioPreset.square,
+                CropAspectRatioPreset.ratio3x2,
+                CropAspectRatioPreset.ratio4x3,
+                CropAspectRatioPreset.ratio5x3,
+                CropAspectRatioPreset.ratio5x4,
+                CropAspectRatioPreset.ratio7x5,
+                CropAspectRatioPreset.ratio16x9
+              ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          title: 'Cropper',
+        ));
+    if (croppedFile != null) {
+      _imageFile = croppedFile;
+    }
+  }
+
+  Future<Null> extractText() async {
+    if (_imageFile == null) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('No Image Found!')));
+      return;
+    }
+    try {
+
+      // Model progress HUD start 
+      setState(() {
+        _saving = true;
+      });
+
+      String _result =
+          await SimpleOcrPlugin.performOCR(_imageFile.path);
+      //converting raw string to json creates problem if image has double quotes in text.
+      // _result = ocrModelFromJson(_resultStringRaw);
+
+      // Model progress HUD stop
+      setState(() {
+        _saving = false;
+      });
+      //Inititalizes dialog box with result string.
+      _editTextController = TextEditingController(text: _result);
+      showDialog(
+          context: context,
+          builder: (_) {
+            return AlertDialog(
+              title: Text('Scanned Text'),
+              actions: [
+                TextButton(
+                    onPressed: () => Share.share(_result),
+                    child: Text('Share'))
+              ],
+              content: TextField(
+                  decoration: InputDecoration(border: InputBorder.none),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  controller: _editTextController),
+            );
+          });
+    } on FormatException {
+      print(_result);
+    } catch (e) {
+      print("exception on OCR operation: ${e.toString()}");
+    }
+  }
+}
+
+class OcrPageButton extends StatelessWidget {
+  final onPressed;
+
+  final text;
+
+  const OcrPageButton({@required this.onPressed, @required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Color(0xff48AA8B)),
+          minimumSize: MaterialStateProperty.all(Size(20, 50))),
+      child: Text(text),
     );
   }
 }
